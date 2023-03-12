02be4c0dda119713e347f57a3e25a433d61596cc
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-12 14:04:09 +0100

    Last Bit of Styling

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import AlexaImage from "./images/alexa.png";
 import CortanaImage from "./images/cortana.png";
 import SiriImage from "./images/siri.png";
 
-console.log(AlexaImage);
-console.log(CortanaImage);
-console.log(SiriImage);
-
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
 
       <div className="container">
         <section className="section">
@@ -21,6 +21,7 @@ function App() {
                 title={"Alexa"}
                 handle={"@alexa99"}
                 image={AlexaImage}
+                description="Alexa was created by Amazon and helps you buy things."
               />
             </div>
             <div className="column is-4">
@@ -28,6 +29,7 @@ function App() {
                 title={"Cortana"}
                 handle={"@cortana32"}
                 image={CortanaImage}
+                description="Cortana was made by Microsoft. Who knows what it does?"
               />
             </div>
             <div className="column is-4">
@@ -35,6 +37,7 @@ function App() {
                 title={"Siri"}
                 handle={"@siri01"}
                 image={SiriImage}
+                description="Siri was made by Apple and is being phased out."
               />
             </div>
           </div>
